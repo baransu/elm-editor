b17fb48a6f9cc53fe19ee5615a2df7abcde0e6df
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2016-09-28 23:40:31 +0200

    add new line, remove last line, modify last line

--- src/Panel.elm
+++ src/Panel.elm
@@ -7,6 +7,7 @@ import Char exposing (..)
 import String exposing (..)
 import Debug exposing (..)
 import Keyboard
+import List exposing (..)
 
 -- MODEL
 
@@ -44,19 +45,55 @@ view model =
 
 -- UPDATE
 
+getLast : List a -> List a
+getLast list =
+    case list of
+        [] -> []
+        l ->
+            case head (List.reverse l) of
+                Nothing -> []
+                Just a -> [a]
+
+removeLast : List a -> List a
+removeLast list =
+    case list of
+        [] -> []
+        l ->
+            List.reverse (drop 1 (List.reverse l))
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update message model =
     case message of
-        -- KeyDownMsg 8 ->
-        --     --( { model | buffer = dropRight 1 model.buffer}, Cmd.none )
-        -- -- ENTER (32 case)
+        KeyDownMsg 8 ->
+            case model.lines of
+                [] ->
+                    ( model, Cmd.none )
+                lines ->
+                    let
+                        last =
+                            case head (getLast lines) of
+                                Nothing -> []
+                                Just a ->
+                                    case dropRight 1 a of
+                                        "" -> []
+                                        a -> [a]
 
-        -- KeyPressedMsg keyCode ->
-        --     let
-        --         --string = log "keyCode" (toString keyCode)
-        --         string = keyToString keyCode
-        --     in
-        --        ( { model | buffer = model.buffer ++ string }, Cmd.none )
+                        lastLess = removeLast lines
+                    in
+                        ( { model | lines = lastLess ++ last }, Cmd.none )
+
+        KeyDownMsg 13 ->
+            ( { model | lines = model.lines ++ [""] }, Cmd.none )
+        KeyPressedMsg keyCode ->
+            let
+                string = keyToString keyCode
+                last =
+                    case head (getLast model.lines) of
+                        Nothing -> ""
+                        Just a -> a
+                lastLess = removeLast model.lines
+            in
+                ( { model | lines = lastLess ++ [(last ++ string)] }, Cmd.none )
 
         _ ->
             ( model, Cmd.none )
